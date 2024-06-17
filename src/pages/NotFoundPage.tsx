612c5ef196b266472e14bdb70539a9796a96d8ff
pandaman404 <valenzuela.ahumada4@gmail.com>
2024-06-17 17:47:30 -0400

    feat:  Error 404 Page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,17 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import notFound from '../assets/error404.svg';
+
 const NotFoundPage = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <section className='flex min-h-screen flex-col justify-between'>
+      <Header />
+      <div className='mx-auto h-auto w-full max-w-80'>
+        <img src={notFound} className='h-full w-full object-cover' />
+      </div>
+      <Footer />
+    </section>
+  );
 };
 
 export default NotFoundPage;
